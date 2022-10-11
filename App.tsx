e67341893850cb292a748333370d424c05b9fa1d
Gabriel Mariano Costa <gabrielmc15@outlook.com>
2022-10-10 23:00:01 -0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar'
+import React, { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const App: FC = () => (
+  <View>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style='auto' />
+  </View>
+)
+
+export default App
